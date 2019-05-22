f5275475a71d71ed4b2799f104d05fe6b4fcc00f
Johvor <33019284+Johvor@users.noreply.github.com>
2019-05-22 10:06:52 +0200

    Upload alt
    Upload alt

---
+++ karrusel.js
@@ -0,0 +1,32 @@
+//Først laves en variabel, der sætter slideIndex til at have værdien 1.
+var slideIndex = 1;
+        showSlides(slideIndex);
+
+//Der laves en funktion med navnet plusSlides
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = slides.length
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
